38817b391b4c999f2bc977f0c44af996ca4005dd
Felix Krückel <hi@felkru.com>
2024-06-06 20:45:31 +0200

    damn. That was way easier than I expected.

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 import streamlit as st
 import pydot
-from IPython.display import Image, display
 from streamlit_tags import st_tags
 import time
+import re
+
+def extract_dicts(input_string):
+    # Define the regex pattern
+    pattern = r'\((\w+),(\w+)\)|(\w+)'
+
+    # Find all matches in the input string
+    matches = re.findall(pattern, input_string)
+
+    result = []
+    for match in matches:
+        if match[0] and match[1]:  # If the match is a pair in parentheses
+            result.append({"label": match[0], "id": match[1]})
+        elif match[2]:  # If the match is a single string
+            result.append({"label": match[2], "id": match[2]})
+
+    return result
 
 "# Jarons Tree Creator"
 
@@ -19,16 +35,20 @@ st.session_state['edges'] = st_tags(
 # Edges for debug purposes
 # st.write(st.session_state['edges'])
 
-for edge in [e.split(",") for e in st.session_state['edges'] if ',' in e]:
-    graph.add_edge(pydot.Edge(pydot.Node(edge[0]), pydot.Node(edge[1])))
+for edge in [e for e in st.session_state['edges'] if ',' in e]:
+    edge = extract_dicts(edge)
+
+    node1 = pydot.Node(edge[0]['id'], label=edge[0]['label'])
+    graph.add_node(node1)
+    node2 = pydot.Node(edge[1]['id'], label=edge[1]['label'])
+    graph.add_node(node2)
+
+    graph.add_edge(pydot.Edge(node1, node2))
 
 "## Preview:"
 # Save the graph to a file
 graph.set_size('"10,10!"')
 graph.write_png('graph.png')
-
-# Display the graph
-display(Image(filename='graph.png'))
 
 st.image('graph.png')
 
