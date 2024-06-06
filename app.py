7b6d37a015ba42fa8e954fe1bdf490abded3cd9b
Felix Krückel <hi@felkru.com>
2024-06-06 21:05:44 +0200

    it's now possible to create empty nodes

--- app.py
+++ app.py
@@ -4,17 +4,19 @@ from streamlit_tags import st_tags
 import time
 import re
 
+
 def extract_dicts(input_string):
     # Define the regex pattern
-    pattern = r'\((\w+),(\w+)\)|(\w+)'
+    pattern = r'\(([^,]*),([^)]*)\)|([^,]+)'
 
     # Find all matches in the input string
     matches = re.findall(pattern, input_string)
 
     result = []
     for match in matches:
-        if match[0] and match[1]:  # If the match is a pair in parentheses
-            result.append({"label": match[0], "id": match[1]})
+        if match[0] or match[1]:  # If the match is a pair in parentheses
+            label = match[0].strip() if match[0].strip() else " "
+            result.append({"label": label, "id": match[1]})
         elif match[2]:  # If the match is a single string
             result.append({"label": match[2], "id": match[2]})
 
@@ -25,15 +27,14 @@ def extract_dicts(input_string):
 # Create the graph
 graph = pydot.Dot("my_graph", graph_type="graph", bgcolor="white")
 
-"## Inputs:"
+"## Input:"
 
 st.session_state['edges'] = st_tags(
-        label="Add edges. Format: 'Node1,Node2':",
-        text="e.g. A,B"
+        text="e.g. A,B or (A, A1),B"
     )
 
 # Edges for debug purposes
-# st.write(st.session_state['edges'])
+# st.session_state['edges']
 
 for edge in [e for e in st.session_state['edges'] if ',' in e]:
     edge = extract_dicts(edge)
