a04bf65b106b3690703e4f152b2a14fec3ba61b9
Felix Krückel <hi@felkru.com>
2024-06-06 16:58:19 +0200

    added input method

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 import streamlit as st
 import pydot
 from IPython.display import Image, display
+from streamlit_tags import st_tags
 
 "# Jarons Tree Creator"
+
+if 'edges' not in st.session_state:
+    st.session_state['edges'] = []
 
 # Create the graph
 graph = pydot.Dot("my_graph", graph_type="graph", bgcolor="white")
 
-graph.add_edge(pydot.Edge("Link A", "Link B", color="black"))
-graph.add_edge(pydot.Edge("Link A", "Link C", color="black"))
-graph.add_edge(pydot.Edge("Link A", "Link D", color="black"))
-graph.add_edge(pydot.Edge("Link B", "-", color="black"))
-graph.add_edge(pydot.Edge("Link C", "Link F", color="black"))
-graph.add_edge(pydot.Edge("Link C", "Link G", color="black"))
-graph.add_edge(pydot.Edge("-", "Link H", color="black"))
-graph.add_edge(pydot.Edge("Link F", "Link I", color="black"))
+"## Inputs:"
+txt_input = st.text_input("Add edges. Format: 'Node1,Node2':", placeholder='A,B')
 
+if txt_input:
+    st.session_state['edges'] += [txt_input.split(",")]
 
+# Edges for debug purposes
+# st.write(st.session_state['edges'])
+
+for edge in st.session_state['edges']:
+    graph.add_edge(pydot.Edge(edge[0], edge[1], color="black"))
+
+st.button("Clear", on_click=lambda: st.session_state.edges.clear())
+
+"## Preview:"
 # Save the graph to a file
 graph.set_size('"10,10!"')
 graph.write_png('graph.png')
@@ -24,11 +33,6 @@ graph.write_png('graph.png')
 # Display the graph
 display(Image(filename='graph.png'))
 
-"## Inputs:"
-
-
-
-"## Preview:"
 st.image('graph.png')
 
 st.download_button(
